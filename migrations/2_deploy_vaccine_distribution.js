9a19e8f247205bfd89af2ed346f62d95c90fa874
Yashashwini Dixit <64366760+YashashwiniDixit@users.noreply.github.com>
2021-05-08 18:10:33 +0530

    Revert "Initial commit"
    This reverts commit 50b69ac29c3396513292e3598b12c4a501f6a63c.

--- migrations/2_deploy_vaccine_distribution.js
+++
@@ -1,5 +0,0 @@
-const VaccineDistribution = artifacts.require("VaccineDistribution");
-
-module.exports = function(deployer) {
-  deployer.deploy(VaccineDistribution);
-};
--- migrations/3_deploy_locations.js
+++
@@ -1,5 +0,0 @@
-const Locations = artifacts.require("Locations");
-
-module.exports = function(deployer) {
-  deployer.deploy(Locations);
-};
